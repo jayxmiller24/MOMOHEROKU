c831595d71f32759b3d92d6b1243b4d4ca387f5c
jxmiller5 <jxmiller5@gmail.com>
2019-05-07 21:04:09 -0700

    SO GOOD

--- Public/resto.js
+++ Public/resto.js
@@ -1,19 +1,35 @@
+
 $("#tables").on("click", function () {
     $.get("/api/tables/", function (data) {
         console.log(data);
-        
+        let tablelist = data;
+        let customerName = data.customerName;
+        let customerEmail = data.customerEmail;
+        let customerID = data.customerID;
+        console.log(tablelist);
+
     });
     $.get("/api/waitlist/", function (data) {
         console.log(data);
+        let waitlist = data;
+        let customerName = data.customerName;
+        let customerEmail = data.customerEmail;
+        let customerID = data.customerID;
+        console.log(waitlist);
     });
 });
 
 $("#submit").on("click", function () {
-    $.get("/api/tables/", function(data) {
-
-    });
-});
-
-$("#home").on("click", function () {
-
+    event.preventDefault();
+    let newTable = {
+        name: $("#name").val().trim(),
+        phone: $("#phone").val().trim(),
+        email: $("#email").val().trim(),
+        id: $("#id").val().trim()
+    };
+    $.post("/api/tables/", newTable)
+        .then(function (data) {
+            console.log("Adding html");
+            $("#reservation-form")[0].reset();
+        });
 });
--- express.js
+++ express.js
@@ -14,24 +14,38 @@ app.use(express.static('public'));
 
 
 //arrays reservations get pushed too//
-var tableList = [];
+var tableList = [
+  {
+    customerName: "Jim Morrison",
+    customerEmail: "lizardking69@yahoo.com",
+    customerID: "blargh01",
+    phoneNumber: "6666666666"
+  }
+];
 
-var waitList = [];
+var waitList = [
+  {
+    customerName: "Van Halen",
+    customerEmail: "panama@gmail.com",
+    customerID: "little12",
+    phoneNumber: "999999999"
+  }
+];
 
 // GET =============================================================
 app.get("/", function(req, res) {
     // res.send("Welcome to our  home page!")
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
   
   app.get("/tables", function(req, res) {
     // res.send("See we only have 5 tables")
-    res.sendFile(path.join(__dirname, "tables.html"));
+    res.sendFile(path.join(__dirname, "/public/tables.html"));
   });
   
   app.get("/reservation", function(req, res) {
     // res.send("Make a reservation")
-    res.sendFile(path.join(__dirname, "reservation.html"));
+    res.sendFile(path.join(__dirname, "/public/reservation.html"));
   });
 
 app.get("/api/tablelist", function(req, res){
